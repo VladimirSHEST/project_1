357343999fee803c94765aae7b1d9e2c1b1cceb1
Vladimir <162320414+VladimirSHEST@users.noreply.github.com>
2025-04-23 21:46:46 +0400

    добавил код

--- src/test/java/task1/ExampleTest.java
+++ src/test/java/task1/ExampleTest.java
@@ -1,2 +1,16 @@
-package task1;public class ExampleTest {
+package task1;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class ExampleTest extends BaseTest {
+    @Test
+    public void testGoogleSearch() {
+        // Открываем страницу Google
+        open("https://www.google.ru/?hl=ru");
+
+    }
 }
